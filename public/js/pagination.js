3d9042c2b3e589c949dd5f8d2b41f2a060dfda0b
MayPhyoKyaw <MayPhyoKyaw@users.noreply.github.com>
2020-12-25 00:33:33 +0630

    pagination

---
+++ public/js/pagination.js
@@ -0,0 +1,27 @@
+var items = $(".list-wrapper .menu-item");
+var numItems = items.length;
+var perPage = 2;
+var pgNumber = 1;
+
+items.slice(perPage).hide();
+
+$("#pagination-container").pagination({
+  items: numItems,
+  itemsOnPage: perPage,
+  prevText: "&laquo;",
+  nextText: "&raquo;",
+  onPageClick: function (pageNumber) {
+    var showFrom = perPage * (pageNumber - 1);
+    var showTo = showFrom + perPage;
+    console.log(showFrom);
+    console.log(showTo);
+    items.hide().slice(showFrom, showTo).show();
+    console.log(location.href);
+  },
+});
+$("#pagination-container").click(function(){
+    pgNumber++
+    console.log(pgNumber);
+})
+console.log(pgNumber);
+document.getElementById("pagination-info").innerHTML = `Total ${numItems} Items`;
